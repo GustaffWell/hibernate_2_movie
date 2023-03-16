836bd8dc0d40045eacf51943676d1a7b08df5133
GustaffWell <gustaff_well95@mail.ru>
2023-03-16 19:09:57 +0500

    Проект по Hibernate 2

---
+++ src/main/java/com/javarush/dao/PaymentDAO.java
@@ -0,0 +1,11 @@
+package com.javarush.dao;
+
+import com.javarush.domain.Payment;
+import org.hibernate.SessionFactory;
+
+public class PaymentDAO extends GenericDAO<Payment>{
+
+    public PaymentDAO(SessionFactory sessionFactory) {
+        super(Payment.class, sessionFactory);
+    }
+}
